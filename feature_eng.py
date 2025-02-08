0f468b0e318ddeae8d7664a4da0a008eb9dfd00f
demir <kubra.demircioglu@tum.de>
2025-02-08 18:03:57 +0300

    SHAP value

--- feature_eng.py
+++ feature_eng.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import preprocess
 from preprocess import DataProcessor, FeatureEngineering
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv(r"C:\Users\demir\OneDrive\Desktop\MSc Thesis\Data\feature_eng\distributed_fuels.csv", delimiter=';')
 data = data.loc[data['devol_yield'] > 0]
@@ -18,8 +19,17 @@ ac_fc = data['ac']/data['fc']
 
 data = data.drop(['h', 'o', 'c', 'wc', 'lhv', 'vm', 'ac', 'fc', 'heat_rate'], axis=1)
 
-# Add new features
 data = pd.concat([data, h_o.rename('H/O'), T_time.rename('T_time'), T_rate.rename('T_rate'), vm_fc.rename('VM/FC'), ac_fc.rename('AC/FC')], axis=1)
-print(data.columns)
 
-data.to_csv(r'C:\Users\demir\OneDrive\Desktop\MSc Thesis\Data\feature_eng\feature_engineered.csv', index=False)
+X_eng = data.drop(['sample','devol_yield'], axis=1)
+y_eng = data['devol_yield']
+
+X_eng_train, X_eng_test, y_eng_train, y_eng_test = train_test_split(X_eng, y_eng, test_size=0.2)
+
+print(f"Train set: X_train={X_eng_train.shape}, y_train={y_eng_train.shape}")
+print(f"Test set: X_test={X_eng_test.shape}, y_test={y_eng_test.shape}")
+
+X_eng_train.to_csv(r'C:\Users\demir\OneDrive\Desktop\MSc Thesis\Data\feature_eng\X_train_engineered.csv', index=False)
+X_eng_test.to_csv(r'C:\Users\demir\OneDrive\Desktop\MSc Thesis\Data\feature_eng\X_test_engineered.csv', index=False)
+y_eng_train.to_csv(r'C:\Users\demir\OneDrive\Desktop\MSc Thesis\Data\feature_eng\y_train_engineered.csv', index=False)
+y_eng_test.to_csv(r'C:\Users\demir\OneDrive\Desktop\MSc Thesis\Data\feature_eng\y_test_engineered.csv', index=False)
